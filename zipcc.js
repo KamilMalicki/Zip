b2b10ab7da94099dabf57bb75b74348e6f74fc1f
KamilMalicki <kamilpusiu2008@gmail.com>
2026-03-30 21:54:36 +0200

    Read from file code

--- zipcc.js
+++ zipcc.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-console.log("Witaj w ZIP kompilerze");
+const fs = require('fs');
+
+let scieska = "";
+
+process.argv.forEach(function (val, index, array) {
+    if (index == 2) {
+        scieska += val;
+    } else if (index > 2) {
+        scieska = scieska + "/" + val;
+    }
+});
+
+try {
+    const dane = fs.readFileSync(scieska, 'utf8');
+    console.log(dane);
+} catch (e) {
+    // coś tu nie gra
+}
